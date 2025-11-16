3c167db7285b54134bce938aa6cf26487df95679
Alexandr Shagov <alexandr.shg01@yandex.ru>
2025-11-16 12:33:45 +0300

    first-commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/serializers/DurationToSecondsSerializer.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.serializers;
+
+public class DurationToSecondsSerializer {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validators/ReleaseAfter1895Validator.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.validators;
+
+public class ReleaseAfter1895Validator {
+}
